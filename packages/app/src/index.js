3f2764517f2df2cecf06613e792bc258a086cbac
bbaresic <153560835+bbaresic@users.noreply.github.com>
2025-01-26 23:33:35 -0600

    add node module

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,5 +1,5 @@
 import app from '@mycompany/app.js';
-// import app from '../../../node_modules/@mycompany/app.js/src/app.public.js';';
+// import app from '../../../node_modules/@mycompany/app.js/src/app.public.js';
 
 
 import '@mycompany/app.js/modules/example';
